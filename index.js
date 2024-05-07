4d61a2c421fea1253e9546a8edfdb0bb239362dd
LILAY <contact@lilay.dev>
2024-05-07 19:47:06 +0700

    remove unused commented code

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-// let request = require("request");
-//
-// request("https://zenquotes.io/api/random", function(err, response, body) {
-//   let bodyJson = JSON.parse(body);
-//   let randomQuote = bodyJson[0]["q"];
-//   document.getElementById("quote").innerHTML = randomQuote;
-//   console.log(randomQuote);
-// });
-//
-// console.log(randomQuote);
-
 fetch("https://zenquotes.io/api/random")
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
     // console.log(data);
     const getQuote = data[0].q;
     const getname = data[0].a;
